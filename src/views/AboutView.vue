18b2f7e5af33f33b73fbded662e35dad00eca5d9
Marcus Vinícius <marcus.varocha@gmail.com>
2023-03-18 01:47:31 +0000

    MID-26: Tela de diretórios

--- src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="dashboard">
-    <h1>This is an about page</h1>
-  </div>
-</template>
-
-<style>
-
-</style>
---
+++ src/views/ConfiguracoesView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import BreadcrumbArrow from '/src/components/icons/BreadcrumbArrow.vue'
+</script>
+
+
+<template>
+  <div class="configuracoes">
+    <div class="breadcrumbs">
+      <p class="active">ESCOLHA O PROVEDOR</p>
+      <BreadcrumbArrow />
+      <p>CONFIGURAÇÕES DA CONEXÃO</p>
+      <BreadcrumbArrow />
+      <p>FLUXO</p>
+      <BreadcrumbArrow />
+      <p>DIRETÓRIOS E CRIAÇÃO</p>
+    </div>
+
+    <h1>1. Selecione o provedor do serviço</h1>
+
+    <div for="googleDrive" class="card">
+      <p>Google Drive</p>
+      <img src="@/assets/images/Google_Drive_logo.png" alt="" srcset="">
+    </div>
+    <input type="radio" name="googleDrive" id="googleDrive">
+    <div for="amazonWebService" class="card">
+      <p>Google Drive</p>
+      <img src="@/assets/images/Google_Drive_logo.png" alt="" srcset="">
+    </div>
+    <input type="radio" name="amazonWebService" id="amazonWebService">
+  </div>
+</template>
+
+<style>
+@import '@/assets/css/configuracoes.css';
+</style>
