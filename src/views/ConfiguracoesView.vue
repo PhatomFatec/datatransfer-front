e00109d5d7b9fee48bef56979ad4a5aabceda2b1
Marcus Vinícius <marcus.varocha@gmail.com>
2023-03-25 00:33:17 +0000

    MID-26: tela de fluxos

--- src/views/ConfiguracoesView.vue
+++ src/views/ConfiguracoesView.vue
@@ -52,9 +52,19 @@ import BreadcrumbArrow from '/src/components/icons/BreadcrumbArrow.vue'
               <img src="@/assets/images/Google_Drive_logo.png" alt="" srcset="" />
             </div>
           </div>
-          <svg width="454" height="38" viewBox="0 0 454 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M452.768 20.7531C453.744 19.7768 453.744 18.1939 452.768 17.2176L436.857 1.30819C435.881 0.331913 434.298 0.331965 433.322 1.30831C432.345 2.28465 432.345 3.86756 433.322 4.84384L447.464 18.9855L433.323 33.1281C432.346 34.1045 432.347 35.6874 433.323 36.6636C434.299 37.6399 435.882 37.6399 436.858 36.6635L452.768 20.7531ZM8.09129e-05 21.5L451 21.4854L451 16.4854L-8.09129e-05 16.5L8.09129e-05 21.5Z" fill="#777171"/>
-</svg>
+
+          <svg
+            width="454"
+            height="38"
+            viewBox="0 0 454 38"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M452.768 20.7531C453.744 19.7768 453.744 18.1939 452.768 17.2176L436.857 1.30819C435.881 0.331913 434.298 0.331965 433.322 1.30831C432.345 2.28465 432.345 3.86756 433.322 4.84384L447.464 18.9855L433.323 33.1281C432.346 34.1045 432.347 35.6874 433.323 36.6636C434.299 37.6399 435.882 37.6399 436.858 36.6635L452.768 20.7531ZM8.09129e-05 21.5L451 21.4854L451 16.4854L-8.09129e-05 16.5L8.09129e-05 21.5Z"
+              fill="#777171"
+            />
+          </svg>
 
           <div class="opcao">
             <div @click="fluxoDirAWS" class="imagem awsimg" id="fluxoDirAWS">
@@ -65,6 +75,10 @@ import BreadcrumbArrow from '/src/components/icons/BreadcrumbArrow.vue'
             </div>
           </div>
         </div>
+        <input type="radio" id="fromDrive" checked>
+        <input type="radio" id="fromAWS">
+        <input type="radio" id="toDrive" checked>
+        <input type="radio" id="toAWS">
       </div>
       <button @click="anterior" class="btn btn-anterior" id="btnAnterior">ANTERIOR</button>
       <button @click="proximo" class="btn btn-proximo" id="btnProximo">PRÓXIMO</button>
@@ -130,13 +144,18 @@ export default {
     fluxoEsqDrive: () => {
       let aws = document.getElementById('fluxoEsqAWS')
       let drive = document.getElementById('fluxoEsqDrive')
+      let fromDrive = document.getElementById('fromDrive')
+      let fromAWS = document.getElementById('fromAWS')
+
+      fromDrive.checked = true
+      fromAWS.checked = false
 
       aws.style = `top: 127px;
                    right: 0px;
                    width: 80px;
                    height: 80px;
                    transition: 0.2s;`
-      
+
       drive.style = `top: 0px;
                      left: 50px;
                      width: 150px;
@@ -146,13 +165,18 @@ export default {
     fluxoEsqAWS: () => {
       let aws = document.getElementById('fluxoEsqAWS')
       let drive = document.getElementById('fluxoEsqDrive')
+      let fromDrive = document.getElementById('fromDrive')
+      let fromAWS = document.getElementById('fromAWS')
+
+      fromDrive.checked = false
+      fromAWS.checked = true
 
       aws.style = `top: 0px;
                    right: 50px;
                    width: 150px;
                    height: 150px;
                    transition: 0.2s;`
-      
+
       drive.style = `top: 127px;
                      left: 0px;
                      width: 80px;
@@ -162,13 +186,18 @@ export default {
     fluxoDirDrive: () => {
       let aws = document.getElementById('fluxoDirAWS')
       let drive = document.getElementById('fluxoDirDrive')
+      let toDrive = document.getElementById('toDrive')
+      let toAWS = document.getElementById('toAWS')
+
+      toDrive.checked = true
+      toAWS.checked = false
 
       aws.style = `top: 127px;
                    right: 0px;
                    width: 80px;
                    height: 80px;
                    transition: 0.2s;`
-      
+
       drive.style = `top: 0px;
                      left: 50px;
                      width: 150px;
@@ -178,13 +207,18 @@ export default {
     fluxoDirAWS: () => {
       let aws = document.getElementById('fluxoDirAWS')
       let drive = document.getElementById('fluxoDirDrive')
+      let toDrive = document.getElementById('toDrive')
+      let toAWS = document.getElementById('toAWS')
+
+      toDrive.checked = false
+      toAWS.checked = true
 
       aws.style = `top: 0px;
                    right: 50px;
                    width: 150px;
                    height: 150px;
                    transition: 0.2s;`
-      
+
       drive.style = `top: 127px;
                      left: 0px;
                      width: 80px;
